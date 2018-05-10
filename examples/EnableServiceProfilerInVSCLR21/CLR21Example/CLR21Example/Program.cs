d920d7b5051e2853f05cf7a970bd1c3553fcd919
Saar Shen <xiaomi7732@hotmail.com>
2018-05-10 13:42:08 -0700

    Enable application insights

--- examples/EnableServiceProfilerInVSCLR21/CLR21Example/CLR21Example/Program.cs
+++ examples/EnableServiceProfilerInVSCLR21/CLR21Example/CLR21Example/Program.cs
@@ -19,6 +19,7 @@ namespace CLR21Example
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
